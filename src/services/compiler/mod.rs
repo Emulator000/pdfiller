8e370fee9b53dcb94947af1b44928b30d5f23372
Emulator000 <emulator@hotmail.it>
2020-09-23 17:09:59 +0200

    Removed merging logic as is not currently possible in Rust

--- src/services/compiler/mod.rs
+++ src/services/compiler/mod.rs
@@ -12,8 +12,7 @@ use serde_json::Value;
 use crate::data::Data;
 use crate::redis::models::document::Document;
 use crate::services::compiler::handler::{
-    get_compiled_filepath, merge_compiled_documents, zip_compiled_documents, ExportCompilerResult,
-    HandlerCompilerResult,
+    get_compiled_filepath, zip_compiled_documents, ExportCompilerResult, HandlerCompilerResult,
 };
 use crate::services::WsError;
 use crate::utils::read_file_buf;
@@ -22,16 +21,10 @@ pub fn config(cfg: &mut web::ServiceConfig) {
     cfg.service(compile_documents);
 }
 
-#[derive(Deserialize)]
-pub struct CompileOptions {
-    pub merge: Option<bool>,
-}
-
 #[post("/compile/{token}")]
 pub async fn compile_documents(
     data: web::Data<Data>,
     token: web::Path<String>,
-    parameters: web::Query<CompileOptions>,
     bytes: web::Bytes,
 ) -> impl Responder {
     match str::from_utf8(&bytes) {
@@ -90,32 +83,15 @@ pub async fn compile_documents(
                                 } else {
                                     match handler::compile_documents(map, &documents) {
                                         HandlerCompilerResult::Success => {
-                                            let merge = parameters.merge.unwrap_or(false);
-                                            let export_result = if merge {
-                                                merge_compiled_documents(&documents)
-                                            } else {
-                                                zip_compiled_documents(&documents)
-                                            };
-
-                                            match export_result {
+                                            match zip_compiled_documents(&documents) {
                                                 ExportCompilerResult::Success(bytes) => {
                                                     HttpResponse::Ok()
                                                         .encoding(ContentEncoding::Identity)
-                                                        .content_type(format!(
-                                                            "{}",
-                                                            if merge {
-                                                                "application/pdf"
-                                                            } else {
-                                                                "application/zip"
-                                                            }
-                                                        ))
+                                                        .content_type("application/zip")
                                                         .header("accept-ranges", "bytes")
                                                         .header(
                                                             "content-disposition",
-                                                            format!(
-                                                                "attachment; filename=\"pdfs.{}\"",
-                                                                if merge { "pdf" } else { "zip" }
-                                                            ),
+                                                            "attachment; filename=\"pdfs.zip\"",
                                                         )
                                                         .body(bytes)
                                                 }
