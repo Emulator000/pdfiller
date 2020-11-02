2b4463bff54800b6edf4243e629e7eff908230f1
Emulator000 <emulator@hotmail.it>
2020-11-02 12:27:24 +0100

    FAG-2674 Accepting certain type of ContentType (ZIP or PDF)

--- src/services/filler/mod.rs
+++ src/services/filler/mod.rs
@@ -5,7 +5,7 @@ mod processor;
 use std::str;
 
 use actix_web::dev::BodyEncoding;
-use actix_web::http::ContentEncoding;
+use actix_web::http::{header::ACCEPT, ContentEncoding};
 use actix_web::{post, web, HttpResponse, Responder};
 
 use serde::Deserialize;
@@ -28,94 +28,111 @@ pub struct CompileOptions {
 pub async fn compile_documents(
     data: web::Data<Data>,
     token: web::Path<String>,
-    parameters: web::Query<CompileOptions>,
+    request: web::HttpRequest,
     bytes: web::Bytes,
 ) -> impl Responder {
     match str::from_utf8(&bytes) {
-        Ok(body) => match serde_json::from_str::<Value>(body) {
-            Ok(values) => {
-                if let Some(value) = values.get("data") {
-                    match <compiler::PDFillerMap>::deserialize(value) {
-                        Ok(ref map) => {
-                            if let Some(documents) =
-                                data.redis.get_all_by::<Document, _>(&token.0).await
-                            {
-                                if documents.is_empty() {
-                                    return HttpResponse::NotFound().json(WsError {
-                                        error: "No documents found for this token!".into(),
-                                    });
-                                }
+        Ok(body) => {
+            match serde_json::from_str::<Value>(body) {
+                Ok(values) => {
+                    if let Some(value) = values.get("data") {
+                        match <compiler::PDFillerMap>::deserialize(value) {
+                            Ok(ref map) => {
+                                if let Some(documents) =
+                                    data.redis.get_all_by::<Document, _>(&token.0).await
+                                {
+                                    if documents.is_empty() {
+                                        return HttpResponse::NotFound().json(WsError {
+                                            error: "No documents found for this token!".into(),
+                                        });
+                                    }
 
-                                match compiler::compile_documents(map, &documents).await {
-                                    compiler::HandlerCompilerResult::Success => {
-                                        let merge = parameters.merge.unwrap_or(false);
-                                        let export_result = if merge {
-                                            compiler::merge_compiled_documents(documents)
-                                        } else {
-                                            compiler::zip_compiled_documents(documents)
-                                        };
+                                    match compiler::compile_documents(map, &documents).await {
+                                        compiler::HandlerCompilerResult::Success => {
+                                            if let Some(accept) = request.headers().get(ACCEPT) {
+                                                let accept =
+                                                    accept.to_str().unwrap_or("").to_lowercase();
 
-                                        match export_result {
-                                            compiler::ExportCompilerResult::Success(bytes) => {
-                                                HttpResponse::Ok()
-                                                    .encoding(ContentEncoding::Identity)
-                                                    .content_type(format!(
-                                                        "{}",
-                                                        if merge {
-                                                            "application/pdf"
-                                                        } else {
-                                                            "application/zip"
-                                                        }
-                                                    ))
-                                                    .header("accept-ranges", "bytes")
-                                                    .header(
-                                                        "content-disposition",
-                                                        format!(
-                                                            "attachment; filename=\"pdfs.{}\"",
-                                                            if merge { "pdf" } else { "zip" }
-                                                        ),
-                                                    )
-                                                    .body(bytes)
-                                            }
-                                            compiler::ExportCompilerResult::Error(message) => {
-                                                HttpResponse::InternalServerError()
-                                                    .json(WsError { error: message })
+                                                if accept.as_str() == mime::APPLICATION_PDF
+                                                    || accept.as_str()
+                                                        == mime::APPLICATION_OCTET_STREAM
+                                                {
+                                                    let export_result = if accept.as_str()
+                                                        == mime::APPLICATION_PDF
+                                                    {
+                                                        compiler::merge_compiled_documents(
+                                                            documents,
+                                                        )
+                                                    } else {
+                                                        compiler::zip_compiled_documents(documents)
+                                                    };
+
+                                                    match export_result {
+                                                    compiler::ExportCompilerResult::Success(
+                                                        bytes,
+                                                    ) => HttpResponse::Ok()
+                                                        .encoding(ContentEncoding::Identity)
+                                                        .content_type(&accept)
+                                                        .header("accept-ranges", "bytes")
+                                                        .header(
+                                                            "content-disposition",
+                                                            format!(
+                                                                "attachment; filename=\"pdf.{}\"",
+                                                                if accept.as_str() == mime::APPLICATION_PDF { "pdf" } else { "zip" }
+                                                            ),
+                                                        )
+                                                        .body(bytes),
+                                                    compiler::ExportCompilerResult::Error(
+                                                        message,
+                                                    ) => HttpResponse::InternalServerError()
+                                                        .json(WsError { error: message }),
+                                                }
+                                                } else {
+                                                    HttpResponse::NotAcceptable().json(WsError {
+                                                        error: "Only PDF or Streams are accepted"
+                                                            .into(),
+                                                    })
+                                                }
+                                            } else {
+                                                HttpResponse::BadRequest().json(WsError {
+                                                    error: "Accept header not set.".into(),
+                                                })
                                             }
                                         }
+                                        compiler::HandlerCompilerResult::FillingError(e) => {
+                                            HttpResponse::BadRequest().json(WsError {
+                                                error: format!(
+                                                    "Error during document filling: {:#?}",
+                                                    e
+                                                ),
+                                            })
+                                        }
+                                        compiler::HandlerCompilerResult::Error(message) => {
+                                            HttpResponse::InternalServerError()
+                                                .json(WsError { error: message })
+                                        }
                                     }
-                                    compiler::HandlerCompilerResult::FillingError(e) => {
-                                        HttpResponse::BadRequest().json(WsError {
-                                            error: format!(
-                                                "Error during document filling: {:#?}",
-                                                e
-                                            ),
-                                        })
-                                    }
-                                    compiler::HandlerCompilerResult::Error(message) => {
-                                        HttpResponse::InternalServerError()
-                                            .json(WsError { error: message })
-                                    }
+                                } else {
+                                    HttpResponse::NotFound().json(WsError {
+                                        error: "No documents found for this token!".into(),
+                                    })
                                 }
-                            } else {
-                                HttpResponse::NotFound().json(WsError {
-                                    error: "No documents found for this token!".into(),
-                                })
                             }
+                            Err(e) => HttpResponse::BadRequest().json(WsError {
+                                error: format!("Not a valid PDFiller request: {:#?}", e),
+                            }),
                         }
-                        Err(e) => HttpResponse::BadRequest().json(WsError {
-                            error: format!("Not a valid PDFiller request: {:#?}", e),
-                        }),
+                    } else {
+                        HttpResponse::BadRequest().json(WsError {
+                            error: "Not a valid PDFiller request.".into(),
+                        })
                     }
-                } else {
-                    HttpResponse::BadRequest().json(WsError {
-                        error: "Not a valid PDFiller request.".into(),
-                    })
                 }
+                Err(e) => HttpResponse::BadRequest().json(WsError {
+                    error: format!("Couldn't decode the body as JSON: {:#?}", e),
+                }),
             }
-            Err(e) => HttpResponse::BadRequest().json(WsError {
-                error: format!("Couldn't decode the body as JSON: {:#?}", e),
-            }),
-        },
+        }
         Err(e) => HttpResponse::InternalServerError().json(WsError {
             error: format!("Error decoding the body: {:#?}", e),
         }),
